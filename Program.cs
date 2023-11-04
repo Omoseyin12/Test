b0db8dae5aa02c8ea14c6b860717cc311ef07654
Omoseyin12 <omoseyindavidtosin@gmail.com>
2023-11-04 17:38:44 +0100

    files

--- Program.cs
+++ Program.cs
@@ -248,6 +248,8 @@ namespace omoseyin
             string s = string.Empty;
             for (int i = 0; i < length; i++)
                 s = String.Concat(s, random.Next(10).ToString());
+
+
             return s;
         }
 
